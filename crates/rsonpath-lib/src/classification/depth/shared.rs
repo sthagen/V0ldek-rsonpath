e076f00684c7fe9a3ce4a3af3e5e27931cb14765
Mat Gienieczko <mat@gienieczko.com>
2026-02-11 03:32:04 +0100

    chore: enable and fix a number of lints (#898)
    * chore: enable and fix a number of lints
    
    Went through the list of optional clippy lints added since the
    project started and enabled some that I considered worthwhile.
    
    This resulted in two major changes:
    - all #[allow] occurrences now have a reason field;
    - all assertions in non-test code have a custom message.
    
    This additionally bumps the MSRV to 1.89 in preparation for #520

--- crates/rsonpath-lib/src/classification/depth/shared.rs
+++ crates/rsonpath-lib/src/classification/depth/shared.rs
@@ -7,7 +7,7 @@ pub(super) mod vector_128;
 #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub(super) mod vector_256;
 
-#[allow(unused_macros)]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 macro_rules! depth_classifier {
     ($name:ident, $core:ident, $vector:ident, $size:literal, $mask_ty:ty) => {
         pub(crate) struct Constructor;
@@ -98,5 +98,5 @@ macro_rules! depth_classifier {
     };
 }
 
-#[allow(unused_imports)]
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
 pub(crate) use depth_classifier;
